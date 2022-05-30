abce01e0e9e54a4900018506c323b899425b06b7
Nebilim1210 <alain.salanie@epita.fr>
2022-05-30 02:19:13 +0200

    Done srvc alert

--- srvc_alert/src/main/scala/Main.scala
+++ srvc_alert/src/main/scala/Main.scala
@@ -1,2 +1,28 @@
+import java.util.Properties
+import org.apache.kafka.clients.consumer._
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import java.time.Duration
+import scala.collection.JavaConverters
+
 object Main extends App {
+
+  val props: Properties = new Properties()
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker1:9092")
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, classOf[StringDeserializer])
+
+  val consumer = new KafkaConsumer[String, String](props)
+  //consumer.subscribe(List("topicname").asJava)
+
+  val records: ConsumerRecords[String, String] = consumer.poll(Duration.ofMillis(100))
+
+  //records.asScala.foreach {record => println(s"Print data here")}
+
+  //consumer.subscribe()
+
+  //val jsonString = os.read(os.pwd/"src"/"test"/"resources"/"phil.json")
+  //val data = ujson.read(jsonString)
+  //data.value // LinkedHashMap("first_name" -> Str("Phil"), "last_name" -> Str("Hellmuth"), "birth_year" -> Num(1964.0))
 }
