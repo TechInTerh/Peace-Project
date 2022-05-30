5ead9b5c03f019b4d7776d180f54a6eac9a64c90
Timothee Ribes <timothee.ribes@epita.fr>
2022-05-30 12:32:06 +0200

    feat(back): simple http4s backend

---
+++ srvc_back/srvc_back/src/main/scala/com/example/srvc_back/Srvc_backRoutes.scala
@@ -0,0 +1,38 @@
+package com.example.srvc_back
+
+import cats.effect.Sync
+import cats.implicits._
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+
+object Srvc_backRoutes {
+
+  def jokeRoutes[F[_]: Sync](J: Jokes[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "joke" =>
+        for {
+          joke <- J.get
+          resp <- Ok(joke)
+        } yield resp
+      case POST -> Root / "joke" =>
+        for {
+          p <- J.get
+          resp <- Ok(p)
+        } yield resp
+    }
+  }
+
+  def helloWorldRoutes[F[_]: Sync](H: HelloWorld[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "hello" / name =>
+        for {
+          greeting <- H.hello(HelloWorld.Name(name))
+          resp <- Ok(greeting)
+        } yield resp
+    }
+  }
+}
