ffcd96190e5b5bd21e42d83261e794cc63751e8a
victor.litoux <victor.litoux@epita.fr>
2022-06-02 20:18:04 +0200

    Feat: add a function to upload a file

--- srvc_analysis/src/main/scala/com/peace/Main.scala
+++ srvc_analysis/src/main/scala/com/peace/Main.scala
@@ -1,9 +1,15 @@
 package com.peace
 
+import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 
 object Main extends App {
+	def add_File(sc: SparkContext, file_path: String,file_name: String): Unit = {
+		println("Adding file: " + file_path)
+		val data = sc.textFile(file_path)
+		data.saveAsTextFile("s3a://spark-test/"+file_name)
+	}
 	val spark = SparkSession.builder().appName("Peace-Analyzer")
 		.master("local[4]")
 		.getOrCreate()
@@ -13,6 +19,11 @@ object Main extends App {
 	sc.hadoopConfiguration.set("fs.s3a.access.key", "minioadmin")
 	sc.hadoopConfiguration.set("fs.s3a.secret.key", "minioadmin")
 	sc.hadoopConfiguration.set("fs.s3a.path.style.access", "true")
+	sc.hadoopConfiguration.set("fs.s3a.multipart.size", "10485760")
+	sc.hadoopConfiguration.set("fs.s3a.fast.upload", "true")
+	add_File(sc,"example_drone_msg/example1.csv","example1.csv")
+	println("Reading file: csv")
 	val df = sc.textFile("s3a://spark-test/")
 	df.foreach(println)
+
 }
