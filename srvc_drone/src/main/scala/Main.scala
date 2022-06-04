ecd027b5ffc8448bc1a421dc0f40e258258c8a68
Alexandre Rulleau <rulleaualexandre@gmail.com>
2022-06-04 14:46:36 +0200

    feat(srvc_drone): Add logs to service, may be converted to real logs later

--- srvc_drone/src/main/scala/Main.scala
+++ srvc_drone/src/main/scala/Main.scala
@@ -6,22 +6,31 @@ import org.apache.kafka.clients.producer._
 
 object Main extends App {
 
+    val TOPIC_NAME = "drone-report"
+    val NB_RECORDS = 100
+
+    println("Start registering AVRO schemas ...")
     registerSchema()
+    println("Finish registering AVRO schemas !")
+
+    println(s"Start producing $NB_RECORDS records on $TOPIC_NAME ...")
 
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:9092")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("schema.resgistry.url", "http://schema-registry:8081")
 
     val producer = new KafkaProducer[String, String](props)
-    val TOPIC = "test"
 
     for (i <- 1 to 50) {
-        val record = new ProducerRecord[String, String](TOPIC, "key", s"hello $i")
+        val record = new ProducerRecord[String, String](TOPIC_NAME, "key", s"hello $i")
         producer.send(record)
     }
 
-    val record = new ProducerRecord[String, String](TOPIC, "key", "the end" + new java.util.Date)
+    val record = new ProducerRecord[String, String](TOPIC_NAME, "key", "the end" + new java.util.Date)
     producer.send(record)
     producer.close()
+
+    println(s"Successfully produced $NB_RECORDS records on $TOPIC_NAME !")
 }
