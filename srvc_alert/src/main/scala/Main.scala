975d00fba40d3dc7d348670597fbd7d298f4c474
Timothee Ribes <timothee.ribes@epita.fr>
2022-06-10 02:37:48 +0200

    feat(alert): GenericAvroSerde working with implicit

--- srvc_alert/src/main/scala/Main.scala
+++ srvc_alert/src/main/scala/Main.scala
@@ -1,14 +1,22 @@
+import io.confluent.kafka.serializers.{ AbstractKafkaAvroSerDeConfig, KafkaAvroSerializer }
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde
+import java.util.Collections
 import java.util.Properties
-import org.apache.kafka.common.serialization.Serdes;
+//import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization._
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.processor.Processor
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Printed
 import org.apache.kafka.streams.kstream.ForeachAction
-import sttp.client3.{HttpURLConnectionBackend, _}
+import org.apache.kafka.streams.kstream.Printed
+import org.apache.kafka.streams.processor.Processor
+import org.apache.kafka.streams.scala.StreamsBuilder;
+import org.apache.kafka.streams.scala.kstream.Consumed._
 import scala.annotation.tailrec
+import sttp.client3.{HttpURLConnectionBackend, _}
+import org.apache.avro.generic.{ GenericData, GenericRecordBuilder, GenericRecord }
+import org.apache.kafka.streams.scala.Serdes._
+import org.apache.kafka.streams.scala.ImplicitConversions._
 
 object Main extends App {
     def send(value: String): Unit = {
@@ -20,19 +28,39 @@ object Main extends App {
     }
 
     val props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe")
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, classOf[GenericAvroSerde])
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, classOf[GenericAvroSerde])
+    props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081")
 
-    val builder = new StreamsBuilder();
-    val source = builder.stream("drone-report")
+    val serdeConfig = Collections.singletonMap(
+          AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+          "http://schema-registry:8081")
+          /*
+    val valueGenericAvroSerde = new GenericAvroSerde()
+    valueGenericAvroSerde.configure(serdeConfig, false)
+    val keyGenericAvroSerde = new GenericAvroSerde()
+    keyGenericAvroSerde.configure(serdeConfig, true)
+    */
+    val builder = new StreamsBuilder()
+    //implicit val consumed = Consumed.if(keyGenericAvroSerde, valueGenericAvroSerde)
+    implicit val genericAvroSerde: Serde[GenericRecord] = {
+        val gas = new GenericAvroSerde
+        val isKeySerde = false
+        gas.configure(serdeConfig, false)
+        gas
+    }
+    val source = builder.stream[String, GenericRecord]("drone-report")
 
-    source.foreach(new ForeachAction[String, String]() {
-        override def apply(key: String, value: String) : Unit = {
-          send(value)
+    source.foreach((k,v)=>send(v.toString()))
+    /*
+    source.foreach(new ForeachAction[String, GenericRecord]() {
+        override def apply(key: String, value: GenericRecord) : Unit = {
+          send(value.toString())
         }
     })
+    */
 
     val topology = builder.build();
 
